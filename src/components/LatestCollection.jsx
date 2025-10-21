100bb739760550ac597ad639cafabe36ee804f4e
Md Hashmi <mdhashmi0711@gmail.com>
2025-10-21 20:44:38 +0530

    Create: ShopContext, LatestCollection component, ProductItem component, Titile component

---
+++ src/components/LatestCollection.jsx
@@ -0,0 +1,24 @@
+import React, { useContext } from 'react'
+import { ShopContext } from '../context/ShopContext'
+import Title from './Title';
+
+const LatestCollection = () => {
+
+    const {products} = useContext(ShopContext);
+    console.log(products)
+
+  return (
+    <div className='my-10'>
+      <div className='text-center py-8 text-3xl'>
+        <Title text1={"LATEST"} text2={"COLLECTIONS"}/>
+        <p className='w-3/4 m-auto text-xs sm:text-sm md:text-base text-gray-600'>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus labore mollitia quasi, voluptate suscipit laudantium consequatur recusandae illum ut.
+        </p>
+      </div>
+      {/*Rendering Products */}
+      
+    </div>
+  )
+}
+
+export default LatestCollection
