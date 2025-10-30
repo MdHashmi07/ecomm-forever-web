c3f3e899132355d1b8a647e571d44824dae7e8a1
Md Hashmi <mdhashmi0711@gmail.com>
2025-10-30 13:04:13 +0530

    feat: create cart page with functinality of add item and remove item, make cartTotal component

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -1,9 +1,80 @@
-import React from 'react'
+import React, { useContext, useEffect, useState } from 'react'
+import { ShopContext } from '../context/ShopContext'
+import Title from '../components/Title'
+import { assets } from '../assets/assets';
+import CartTotal from '../components/CartTotal';
 
 const Cart = () => {
+  const { products, currency, cartItems, updateQuantity, navigate } = useContext(ShopContext);
+  const [cartData, setCartData] = useState([]);
+
+  useEffect(() => {
+
+    const tempData = [];
+    for (const items in cartItems) {
+      for (const item in cartItems[items]) {
+        if (cartItems[items][item] > 0) {
+          tempData.push({
+            _id: items,
+            size: item,
+            quantity: cartItems[items][item]
+          })
+        }
+      }
+    }
+    setCartData(tempData);
+
+  }, [cartItems]);
+
+
   return (
-    <div>
-      
+    <div className='border-t pt-14'>
+      <div className='text-2xl mb-3'>
+        <Title text1={'YOUR'} text2={'CART'} />
+      </div>
+      <div>
+        {
+          cartData.map((item, index) => {
+            const productData = products.find((product) => product._id === item._id);
+
+            return (
+              <div key={index} className='py-4 border-t border-b text-gray-700 grid grid-cols-[4fr_0.5fr_0.5fr] sm:grid-cols-[4fr_2fr_0.5fr] items-center gap-4'>
+                <div className='flex items-start gap-6'>
+                  <img className='w-16 sm:w-20' src={productData.image[0]} alt="product img" />
+                  <div>
+                    <p className='text-xs sm:text-lg font-medium'>{productData.name}</p>
+                    <div className='flex items-center gap-5 mt-2'>
+                      <p>{currency}{productData.price}</p>
+                      <p className='px-2 sm:px-3 sm:py-1 border bg-slate-50'>{item.size}</p>
+                    </div>
+                  </div>
+                </div>
+                <input onChange={(e) => e.target.value === '' || e.target.value === '0' ? null : updateQuantity(item._id, item.size, Number(e.target.value))} className='border max-w-10 sm:max-w-20 px-1 sm:px-2 py-1' type="number" min={1} defaultValue={item.quantity} />
+                <img onClick={() => updateQuantity(item._id, item.size, 0)} className='w-4 mr-4 sm:w-5 cursor-pointer' src={assets.bin_icon} alt="bin icon" />
+              </div>
+            )
+          })
+        }
+      </div>
+      {cartData.length > 0 ? <div className='flex justify-end my-20'>
+        <div className='w-full sm:w-[450px]'>
+          <CartTotal />
+          <div className='w-full text-center sm:text-end'>
+            <button onClick={() => navigate('/place-order')} className='bg-black text-white my-8 px-8 py-3'>PROCEED TO PAYMENT</button>
+          </div>
+        </div>
+      </div>
+        :
+        <div className='w-full h-[50vh] sm:h-[80vh] flex flex-col justify-center items-center'>
+          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-10 sm:size-16 mb-3 sm:mb-5">
+            <path strokeLinecap="round" strokeLinejoin="round" d="M15.75 10.5V6a3.75 3.75 0 1 0-7.5 0v4.5m11.356-1.993 1.263 12c.07.665-.45 1.243-1.119 1.243H4.25a1.125 1.125 0 0 1-1.12-1.243l1.264-12A1.125 1.125 0 0 1 5.513 7.5h12.974c.576 0 1.059.435 1.119 1.007ZM8.625 10.5a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Zm7.5 0a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Z" />
+          </svg>
+
+          <p className='text-lg mb-2'>Your Bag is Empty!</p>
+          <p className='text-xs sm:text-sm text-gray-500'>There is nothing in your bag. Let's add some items.</p>
+        </div>
+      }
+
     </div>
   )
 }
--- src/pages/Product.jsx
+++ src/pages/Product.jsx
@@ -24,6 +24,8 @@ const Product = () => {
   useEffect(() => {
     fetchproductData();
   }, [productId, products])
+
+  
   return productData ? (
     <div className='border-t-2 pt-10 transition-opacity ease-in duration-500 opacity-100'>
       {/*---------------Product Data---------------- */}
