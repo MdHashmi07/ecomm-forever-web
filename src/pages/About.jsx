66651c2f26d392a9b3a40b04ff1f4bba9e210c26
Md Hashmi <mdhashmi0711@gmail.com>
2025-11-18 12:07:52 +0530

    complete about us page and create contact us page

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { assets } from '../assets/assets'
 import Title from '../components/Title'
+import NewsLetterBox from '../components/NewsLetterBox'
 
 const About = () => {
   return (
@@ -18,6 +19,27 @@ const About = () => {
             <p>Our Mission  at Forever is to empower customers with choice. Convenience, and confidence. We're dedicated to providing a seamless shopping experince that exceed expectations, from browsing and ordering to delivery and beyound.</p>
           </div>
       </div>
+
+      <div className='text-4xl py-4'>
+        <Title text1={'WHY'} text2={'CHOOSE US'}/>
+      </div>
+
+      <div className='flex flex-col md:flex-row text-sm mb-20'>
+        <div className='border px-10 md:px-16 py-8 sm:py-20 flex flex-col gap-5'>
+          <b>Quality Assurance: </b>
+          <p className='text-gray-600'>We meticulously select and vet each product to ensure it meets our stringent quality standards.</p>
+        </div>
+        <div className='border px-10 md:px-16 py-8 sm:py-20 flex flex-col gap-5'>
+          <b>Convenience: </b>
+          <p className='text-gray-600'>With our user-friendly interface abd hassle-free ordering process, shopping has never been easier. </p>
+        </div>
+        <div className='border px-10 md:px-16 py-8 sm:py-20 flex flex-col gap-5'>
+          <b>Exceptional Customer Service: </b>
+          <p className='text-gray-600'>Our team of dedicated professionals is here to assists you the way, ensuring your satisfaction is our top priority. </p>
+        </div>
+      </div>
+
+      <NewsLetterBox/>
     </div>
   )
 }
